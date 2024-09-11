b16497a1557d3e9d0a4b7f71934fae410f9d3576
Sanjeevani <sanjeevanimangla09@gmail.com>
2024-09-11 17:18:43 +0530

    First Commit

--- src/config/msalConfig.js
+++ src/config/msalConfig.js
@@ -1,14 +1,14 @@
 export const msalConfig = {
   auth: {
-    clientId: 'xxxx-xxxx-xxxx-xxxx-xxxxx', // This is the ONLY mandatory field that you need to supply.
-    authority: 'https://<tenant-name>.b2clogin.com/<tenant-name>.onmicrosoft.com/b2c_1_susi', // Choose SUSI as your default authority.
-    knownAuthorities: ['<tenant-name>.b2clogin.com'], // Mark your B2C tenant's domain as trusted.
-    redirectUri: 'http://localhost:3000/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-    postLogoutRedirectUri: 'http://localhost:3000/', // Indicates the page to navigate after logout.
-    navigateToLoginRequestUrl: false, // If 'true', will navigate back to the original request location before processing the auth code response.
+    clientId: '41a51533-79e9-4088-b6e2-41c8972d7de5', 
+    authority: 'https://trackexpense.b2clogin.com/trackexpense.onmicrosoft.com/B2C_1_expense_tracker', 
+    knownAuthorities: ['trackexpense.b2clogin.com'], 
+    redirectUri: 'http://localhost:3000/', 
+    postLogoutRedirectUri: 'http://localhost:3000/', 
+    navigateToLoginRequestUrl: false, 
   },
   cache: {
-    cacheLocation: 'sessionStorage', // Configures cache location. 'sessionStorage' is more secure, but 'localStorage' gives you SSO between tabs.
-    storeAuthStateInCookie: false, // Set this to 'true' if you are having issues on IE11 or Edge
+    cacheLocation: 'sessionStorage', 
+    storeAuthStateInCookie: false, 
   }
 }
